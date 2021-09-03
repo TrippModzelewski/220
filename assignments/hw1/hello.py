44b60863c1ec07eb43794678c51bfbd3401423fb
tripp <modzelewskitr@g.cofc.edu>
2021-09-03 12:51:23 -0400

    hello.py

--- assignments/hw1/hello.py
+++ assignments/hw1/hello.py
@@ -1,6 +1,11 @@
 """
 Name: Tripp Modzelewski
 hello.py
+
+Problem: This function will print "hello, world!" to your screen
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work
 """
 
 
