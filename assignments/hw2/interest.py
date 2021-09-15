6071b36e8bb617243d6021ac10220aa4296390aa
tripp <modzelewskitr@g.cofc.edu>
2021-09-15 14:19:40 -0400

    interest.py

--- assignments/hw2/interest.py
+++ assignments/hw2/interest.py
@@ -10,18 +10,16 @@ I certify that this assignment is entirely my own work
 
 
 def main():
+    rate = eval(input("Input the annual interest rate: "))
+    days = eval(input("Input total number of days in billing cycle: "))
     previous_balance = eval(input("Input net balance shown on billing statement: "))
     payment = eval(input("Input net payment received: "))
-    days = eval(input("Input total number of days in billing cycle: "))
     payment_day = eval(input("Input how many days in which the bill paid: "))
-    rate = eval(input("Input the monthly annual interest rate: "))
-    m_interest = rate / 12
     step_1 = previous_balance * days
     step_2 = payment * (days - payment_day)
-    step_3 = step_2 - step_1
-    average_daily_balance = step_3 / days
-    interest_charge = average_daily_balance * m_interest
-    print("average monthly interest charge:", interest_charge)
+    step_3 = (step_1 - step_2)
+    step_4 = (step_3 / days)
+    print(round(step_4 * (rate / 1200), 2))
 
     if __name__ == "__main__":
         main()
